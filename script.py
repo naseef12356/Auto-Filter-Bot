531dba2fcbc299b5b3415d6d5b0b4da7a04d4dbf
naseef12356 <80581006+naseef12356@users.noreply.github.com>
2021-03-15 05:52:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,35 +3,20 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-
-So no need to add filters for your files or movies from now on!
-
+Use me Go to @CFCHART 
 For more click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 <i>    
-* Add user account to your channel !
+To Use Me go to @CFCHART</i>
 
-* Add bot to group with admin rights.
-
-* Once deployed and bot is started, send a message to channel (must do step)
-
-* Bot will work now :)
-
-No need add each filter again!
-Bot will automatically search for your file and give links to that!
-</i>
-
-<b>© @TroJanzHEX</b>"""
+<b>© @CFCHART</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @CFCHART 
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
